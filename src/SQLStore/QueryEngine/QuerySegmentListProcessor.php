7cdba341625bb14e377e05e7d729b1337b880fcb
海屿有燕 <zorua@vip.qq.com>
2025-06-29 19:49:29 +0800

    移除查询中的反引号以优化 MediaWiki DBAL 的性能

--- src/SQLStore/QueryEngine/QuerySegmentListProcessor.php
+++ src/SQLStore/QueryEngine/QuerySegmentListProcessor.php
@@ -340,7 +340,7 @@ class QuerySegmentListProcessor {
 
 		// Try to safe time (SELECT is cheaper than creating/dropping 3 temp tables):
 		$res = $this->connection->select(
-			$smwtable, // Remove backticks for MediaWiki DBAL
+			$smwtable,
 			's_id',
 			$valuecond,
 			__METHOD__,
